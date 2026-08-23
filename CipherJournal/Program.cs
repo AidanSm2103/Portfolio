97787b428d748d758a45cfbbc453d0ecb2b1e1bf
Aidan Smith <aidansm2103@gmail.com>
2026-08-23 21:45:06 +0200

    Fixed cors origin to bare domain

--- CipherJournal/Program.cs
+++ CipherJournal/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("https://aidansm2103.github.io/Portfolio/")
+        policy.WithOrigins("https://aidansm2103.github.io")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
