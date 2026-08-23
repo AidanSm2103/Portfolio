e9198a50eb445e178912098358ba226dd85ddf79
Aidan Smith <aidansm2103@gmail.com>
2026-08-23 21:15:13 +0200

    Added the origin URL for github pages frontend

--- CipherJournal/Program.cs
+++ CipherJournal/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.AllowAnyOrigin()
+        policy.WithOrigins("https://aidansm2103.github.io/Portfolio/")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
